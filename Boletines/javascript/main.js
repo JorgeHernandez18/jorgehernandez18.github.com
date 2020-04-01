92922ac6bbe3c898dfc5232d903c2edc1cd57f50
JorgeHernandez18 <jorgekevinhl@ufps.edu.co>
2020-03-31 21:43:47 -0500

    Agregamos el proyecto Boletines
    El proyecto Boletines indican los lugares donde venden hamburguesas preferidas de Jorge en la ciudadela de Juan Atalaya.

---
+++ Boletines/javascript/main.js
@@ -0,0 +1,51 @@
+//DOM
+//querySelector / querySelectorAll
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+	console.log(link);
+});
+*/
+
+/*let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+	td.addEventListener('click',function(){
+		console.log(this);
+	});
+});*/
+
+//Obtener elementos de la clase .close
+let links = document.querySelectorAll(".close");
+//recorrelos
+	links.forEach(function(link){
+//Agregar un evento click a cada uno de ellos
+	link.addEventListener("click",function(e){
+		e.preventDefault();
+		let content = document.querySelector(".content");
+		//Quitarle clases de animación 
+		content.classList.remove("heartBeat");
+		content.classList.remove("animated");
+
+		//Agregar clases de animación de salida
+		content.classList.add("fadeOutUp");
+		content.classList.add("animated");
+
+		//setTimeout despues de cierto tiempo una vez
+		//setInterval despues de cierto tiempo constantemente
+		setTimeout(function(){
+			location.href = "/";
+		},600);
+		
+
+
+		return false;
+	});
+});
+
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+	icono.classList.remove("fa-star");
+});		
